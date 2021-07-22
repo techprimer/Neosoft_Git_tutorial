029290fd2b315dad866b4bdf64edac639ec21755
yashpatel5929 <yashp6765@gmail.com>
2021-07-22 12:29:59 +0530

    changed println line in summation.jsvs file

--- summation.java
+++ summation.java
@@ -2,6 +2,6 @@ public class summation{
 	public static void main(string args[])
 		int num1 =10 , num=20 , sum;
 		sum = num1 + num2;
-		system.out.println("sum :" + sum);
+		system.out.println("sum two number is :" + sum);
 	}
 }
